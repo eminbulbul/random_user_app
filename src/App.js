a632c3ddd3a841cfae276e58d963b78af84e14ec
m_emin <mehmeteminbul@gmail.com>
2022-04-09 00:28:22 +0300

    thanks to meTeam

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Assets from "./assets/Assets";
+import Assetsmui from "./assets/Assetsmui";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Assets /> */}
+      <Assetsmui />
+    </div>
+  );
+}
+
+export default App;
